b98156300934ccc1e86bffcef81103b12e8e8149
Nikhil Partap Singh <nikhilpartapsingh88@gmail.com>
2025-11-23 00:28:01 +0530

    Time: 8 ms (70.43%) | Memory: 19.4 MB (12.69%) - LeetSync

--- leet/759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.py
+++ leet/759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.py
@@ -1,23 +1,19 @@
 class Solution:
     def intersectionSizeTwo(self, intervals: List[List[int]]) -> int:
         intervals.sort(key=lambda x: (x[1], -x[0]))
-        count = 0
-        p1 = -1  # The second largest number chosen
-        p2 = -1  # The largest number chosen
-        
-        for s, e in intervals:
-            # Case 1: No overlap at all
-            if s > p2:
-                count += 2
-                p1 = e - 1
-                p2 = e
-            
-            # Case 2: One number overlaps (p2 is inside, p1 is outside)
-            elif s > p1:
-                count += 1
-                p1 = p2  # The old max becomes the 2nd max
-                p2 = e   # The new max is the current end
-            
-            # Case 3: Both p1 and p2 are inside [s, e]. Do nothing.
-            
-        return count
+        out = 0
+
+        pt1 = -1
+        pt2 = -1
+
+        for start , end in intervals:
+
+            if start > pt2:
+                out += 2
+                pt1 = end -1
+                pt2 = end
+            elif start > pt1:
+                out += 1
+                pt1 = pt2
+                pt2 = end
+        return out
